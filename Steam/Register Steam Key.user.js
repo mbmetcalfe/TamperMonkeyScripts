60a4c64886c81443a7423f72450babcfaf06439b
mbmetcalfe <30930387+mbmetcalfe@users.noreply.github.com>
2018-05-10 20:50:02 -0300

    Updating version.

--- Steam/Register Steam Key.user.js
+++ Steam/Register Steam Key.user.js
@@ -2,7 +2,7 @@
 // @name            Register Steam Key
 // @namespace       http://tampermonkey.net/
 // @description     Goes to the Steam register key page using the selected text as the key.
-// @version         0.1
+// @version         0.2
 // @author          That Guy (https://github.com/mbmetcalfe)
 // @include         https://www.indiegala.com/profile/*
 // @include         https://www.reddit.com/*
